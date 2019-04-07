d9088763db4ea7515c63a3c795780292a980d35a
Gabriel Carvalho <gabrielksi@outlook.com>
2019-04-07 18:29:44 -0300

    valor medio adicionado

--- classes/OrdenadorClasses/Ordenador.java
+++ classes/OrdenadorClasses/Ordenador.java
@@ -30,14 +30,28 @@ public class Ordenador {
 	}
 	
 	void inserirColecao(Collection<String> pColecao) {
+		
 		String maiorValor = "0";
+		String menorValor = "999999";
+		double soma = 0;
+		double tamanho = 0;
 		
 		for(String valor : pColecao) {
 			if(Double.parseDouble(valor) > Double.parseDouble(maiorValor)) {
 				maiorValor = valor;
 				this.maiorValor = Double.parseDouble(maiorValor);
 			}
+			if(Double.parseDouble(valor) > Double.parseDouble(menorValor)) {
+				menorValor = valor;
+				this.menorValor = Double.parseDouble(menorValor);
+			}
+			if(valor == "") {
+				soma = soma + Double.parseDouble(valor);
+			}
 		}
+		tamanho = pColecao.size();
+		this.valorMedio = soma / tamanho;
+			
 	}
 	
 	void informarValor(String pValor) {
